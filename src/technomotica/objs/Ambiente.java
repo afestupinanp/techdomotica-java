b10e173e7d0deab56862640a315099d9cfc2fff4
xman40100 <andres_felipe_2000@hotmail.com>
2019-02-21 08:47:37 -0500

    Sincronizado con la rama de Andres

--- src/technomotica/objs/Ambiente.java
+++ src/technomotica/objs/Ambiente.java
@@ -1,66 +1,46 @@
 package technomotica.objs;
 
-import java.util.ArrayList;
-import technomotica.objs.comps.Componente;
+import technomotica.objs.comps.*;
 
 public class Ambiente {
     
-    private String nombre = "",
-                  centro = "";
+    private String perfilActual = "";
     private Admin adminEncargado = null;
     
-    private ArrayList<Componente> componentes = null;
+    private ACondicionado[] acondicionado = new ACondicionado[2];
+    private Camara[] camaras = new Camara[4];
+    private Luz[] luces = new Luz[12];
+    private Sensor[] sensores = new Sensor[4];
+    private Televisor proyector = null;
     
-    public Ambiente(String name, String centre, ArrayList<Componente> comps) {
-        nombre = name;
-        centro = centre;
-        componentes = comps;
-        adminEncargado = null;
-    }
-    
-    public Ambiente(String name, String centre, ArrayList<Componente> comps, Admin encargado) {
-        nombre = name;
-        centro = centre;
-        componentes = comps;
+    public Ambiente(Admin encargado) {
         adminEncargado = encargado;
     }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getCentro() {
-        return centro;
-    }
-
-    public void setCentro(String centro) {
-        this.centro = centro;
-    }
-
-    public Admin getAdminEncargado() {
-        return adminEncargado;
-    }
-
-    public void setAdminEncargado(Admin adminEncargado) {
-        this.adminEncargado = adminEncargado;
-    }
-
-    public ArrayList<Componente> getComponentes() {
-        return componentes;
-    }
-    
-    public Componente getComponente(int index) {
-        return componentes.get(index);
-    }
-
-    public void setComponentes(ArrayList<Componente> componentes) {
-        this.componentes = componentes;
-    }
     
     
     
+    public void cargarPerfil() {
+        //Conectar con = new Conectar();
+        //con.consultarbd("SELECT * FROM ...");
+    }
+    
+    public ACondicionado getACondicionado(int index) {
+        return acondicionado[index];
+    }
+    
+    public Camara getCamara(int index) {
+        return camaras[index];
+    }
+    
+    public Luz getLuz(int index) {
+        return luces[index];
+    }
+    
+    public Sensor getSensor(int index) {
+        return sensores[index];
+    }
+    
+    public Televisor getTelevisor() {
+        return proyector;
+    }
 }
