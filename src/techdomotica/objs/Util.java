49bb5c2433afce64d232e09659061ee89e50e372
xman40100 <andres_felipe_2000@hotmail.com>
2019-04-16 11:28:52 -0500

    Declaración de la versión 0.4.

--- src/techdomotica/objs/Util.java
+++ src/techdomotica/objs/Util.java
@@ -6,7 +6,7 @@ package techdomotica.objs;
  */
 public class Util {
     
-    public static final String VERSION = "0.3.1";
+    public static final String VERSION = "0.4";
     
     /**
      * Chequea si el String está vacío.
