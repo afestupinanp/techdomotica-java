486778acaccafa9a85f7e2a0229bc8f8c7c1c9fe
Andrés Felipe <xman40100@users.noreply.github.com>
2019-05-17 21:37:05 -0500

    Tech Domótica SQL v0.6

--- techdomotica.sql
+++ techdomotica.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.8.0.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: localhost
--- Tiempo de generación: 16-05-2019 a las 14:48:46
--- Versión del servidor: 10.1.39-MariaDB
--- Versión de PHP: 7.3.5
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 18-05-2019 a las 04:32:24
+-- Versión del servidor: 10.1.32-MariaDB
+-- Versión de PHP: 5.6.36
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -85,12 +85,16 @@ CREATE TABLE `luz` (
 
 CREATE TABLE `perfil` (
   `id_perfil` int(11) NOT NULL,
-  `id_Acondicionado` int(11) NOT NULL,
-  `id_tv` int(11) NOT NULL,
-  `id_camara` int(11) NOT NULL,
-  `id_sensor` int(11) NOT NULL,
-  `id_luz` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id_usuario` int(11) NOT NULL,
+  `temp1` int(11) DEFAULT '0',
+  `temp2` int(11) DEFAULT '0',
+  `temp1_on` tinyint(4) DEFAULT '0',
+  `temp2_on` tinyint(4) DEFAULT '0',
+  `proyector_on` tinyint(4) DEFAULT '0',
+  `sensor1_on` tinyint(1) DEFAULT '0',
+  `sensor2_on` tinyint(1) DEFAULT '0',
+  `habilitado` tinyint(1) DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -106,16 +110,6 @@ CREATE TABLE `reporte` (
   `hora` time NOT NULL,
   `texto` varchar(1000) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `reporte`
---
-
-INSERT INTO `reporte` (`id_reporte`, `id_usuario`, `id_tr`, `fecha`, `hora`, `texto`) VALUES
-(1, 1, 7, '2019-05-16', '07:40:36', 'Intento de inicio de sesión en la versión de Java en Linux'),
-(2, 1, 7, '2019-05-16', '07:41:49', 'Intento de inicio de sesión en la versión de Java en Linux'),
-(3, 1, 9, '2019-05-16', '07:42:06', 'Se ha realizado un cambio de contraseña exitoso para este usuario desde la versión de Java en Linux.'),
-(4, 1, 1, '2019-05-16', '07:42:12', 'Este usuario ha iniciado sesión en la versión de Java desde Linux');
 
 -- --------------------------------------------------------
 
@@ -173,9 +167,7 @@ INSERT INTO `tipo_reporte` (`id_tr`, `nombre`) VALUES
 (6, 'Cierre de sesión'),
 (7, 'Intento de inicio de sesión'),
 (8, 'Personalizado'),
-(9, 'Cambio de contraseña'),
-(10, 'Usuario deshabilitado'),
-(11, 'Dispositivo deshabilitado');
+(9, 'Cambio de contraseña');
 
 -- --------------------------------------------------------
 
@@ -206,6 +198,7 @@ CREATE TABLE `usuario` (
   `apellido1` varchar(30) NOT NULL,
   `apellido2` varchar(30) NOT NULL,
   `dni` int(30) NOT NULL,
+  `perfil_actual` int(11) DEFAULT NULL,
   `habilitado` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -213,8 +206,8 @@ CREATE TABLE `usuario` (
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`id_usuario`, `id_rol`, `correo`, `password`, `nom1`, `nom2`, `apellido1`, `apellido2`, `dni`, `habilitado`) VALUES
-(1, 1, 'admin@admin.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 'Andrés', 'Felipe', 'Estupiñán', 'Peláez', 123456, 1);
+INSERT INTO `usuario` (`id_usuario`, `id_rol`, `correo`, `password`, `nom1`, `nom2`, `apellido1`, `apellido2`, `dni`, `perfil_actual`, `habilitado`) VALUES
+(1, 1, 'andres.pelaez00@hotmail.com', '0a92efb1b91ac02c858ab205fbb6baf44d67e8d1e625600a11020cfae50065da', 'Andrés', 'Felipe', 'Estupiñán', 'Peláez', 1005964948, 1, 1);
 
 --
 -- Índices para tablas volcadas
@@ -253,11 +246,7 @@ ALTER TABLE `luz`
 --
 ALTER TABLE `perfil`
   ADD PRIMARY KEY (`id_perfil`),
-  ADD KEY `id_Acondicionado` (`id_Acondicionado`),
-  ADD KEY `id_tv` (`id_tv`),
-  ADD KEY `id_camara` (`id_camara`),
-  ADD KEY `id_sensor` (`id_sensor`),
-  ADD KEY `id_luz` (`id_luz`);
+  ADD KEY `fk_usuario_perfil` (`id_usuario`);
 
 --
 -- Indices de la tabla `reporte`
@@ -298,7 +287,8 @@ ALTER TABLE `tv`
 --
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`id_usuario`),
-  ADD KEY `id_rol` (`id_rol`);
+  ADD KEY `id_rol` (`id_rol`),
+  ADD KEY `perfil_actual` (`perfil_actual`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -338,7 +328,7 @@ ALTER TABLE `perfil`
 -- AUTO_INCREMENT de la tabla `reporte`
 --
 ALTER TABLE `reporte`
-  MODIFY `id_reporte` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_reporte` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `rol`
@@ -356,7 +346,7 @@ ALTER TABLE `sensor`
 -- AUTO_INCREMENT de la tabla `tipo_reporte`
 --
 ALTER TABLE `tipo_reporte`
-  MODIFY `id_tr` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id_tr` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `tv`
@@ -402,11 +392,7 @@ ALTER TABLE `luz`
 -- Filtros para la tabla `perfil`
 --
 ALTER TABLE `perfil`
-  ADD CONSTRAINT `perfil_ibfk_1` FOREIGN KEY (`id_Acondicionado`) REFERENCES `acondicionado` (`id_Acondicionado`),
-  ADD CONSTRAINT `perfil_ibfk_2` FOREIGN KEY (`id_tv`) REFERENCES `tv` (`id_tv`),
-  ADD CONSTRAINT `perfil_ibfk_3` FOREIGN KEY (`id_camara`) REFERENCES `camara` (`id_camara`),
-  ADD CONSTRAINT `perfil_ibfk_4` FOREIGN KEY (`id_sensor`) REFERENCES `sensor` (`id_sensor`),
-  ADD CONSTRAINT `perfil_ibfk_5` FOREIGN KEY (`id_luz`) REFERENCES `luz` (`id_luz`);
+  ADD CONSTRAINT `fk_usuario_perfil` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Filtros para la tabla `reporte`
@@ -422,16 +408,11 @@ ALTER TABLE `sensor`
   ADD CONSTRAINT `sensor_ibfk_1` FOREIGN KEY (`id_componente`) REFERENCES `componente` (`id_componente`);
 
 --
--- Filtros para la tabla `tv`
---
-ALTER TABLE `tv`
-  ADD CONSTRAINT `tv_ibfk_1` FOREIGN KEY (`id_componente`) REFERENCES `componente` (`id_componente`);
-
---
 -- Filtros para la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`id_rol`) REFERENCES `rol` (`id_rol`);
+  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`id_rol`) REFERENCES `rol` (`id_rol`),
+  ADD CONSTRAINT `usuario_ibfk_2` FOREIGN KEY (`perfil_actual`) REFERENCES `perfil` (`id_perfil`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
